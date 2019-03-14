4b50cc4fef59464673806599247ba5be65008cb5
Anthony Lardy <lardy.anthony@hotmail.com>
2019-03-14 11:05:39 +0100

    final commit

--- morpionTest/app/src/main/java/com/example/fisso/morpiontest/Credits.java
+++ morpionTest/app/src/main/java/com/example/fisso/morpiontest/Credits.java
@@ -11,23 +11,11 @@ public class Credits  extends AppCompatActivity {
 
 
 
-    ImageButton retour;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.credits);
 
-        retour = (ImageButton) findViewById(R.id.boutonAccueil);
-
-
-        retour.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //on fait appel à la méthode ouvrirMainActivity
-                ouvrirAccueilActivity();
-            }
-        });
     }
 
 
